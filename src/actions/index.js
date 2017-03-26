3dd72f8497c4159ea849163bb45471bfbe012ede
Reza Zacky Ramadan <rezazramadhan@gmail.com>
2017-03-26 14:47:19 +0700

    Add files via upload

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '9f95788aca74f255835a1d80b8fcb4b3';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+    
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
